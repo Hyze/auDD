d74cec991861654984fced711b6415e125d43212
Fausto Lopez <faustoec1@icloud.com>
2019-09-18 19:08:27 +0200

    Reparation classe five

--- example-java-maven/src/main/java/example/FiveTest.java
+++ example-java-maven/src/main/java/example/FiveTest.java
@@ -2,34 +2,42 @@ package example;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FiveTest {
 
+	public static Five five;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		five = new Five();
+	}
+	
     private static int test = 5;
 
     @Test
     public void testIntToString() {
 	// test pour intToString()
-	assertEquals("5", intToString(test));
+	assertEquals("5", five.intToString(test));
     }
 
     @Test
     public void testIntToString1() {
 	// test pour intToString()
-	assertEquals("5", intToString1(test));
+	assertEquals("5", five.intToString1(test));
     }
 
     @Test
     public void testIntToString2() {
 	// test pour intToString()
-	assertEquals("5", intToString2(test));
+	assertEquals("5", five.intToString2(test));
     }
 
     @Test
     public void testIntToString3() {
 	// test pour intToString()
-	assertEquals("5", intToString3(test));
+	assertEquals("5", five.intToString3(test));
     }
 
 }
