38a5268133263d4f2a6829214c0f804e07f2bc29
Fausto Lopez <faustoec1@icloud.com>
2019-09-18 18:25:50 +0200

    FourTest

---
+++ example-java-maven/src/main/java/example/FourTest.java
@@ -0,0 +1,37 @@
+package example;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class FourTest {
+
+	public static Four four;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		four = new Four();
+	}
+
+	@Test
+	public void testGetMessage() {
+		assertEquals(four.message,four.getMessage());
+	}
+	
+	@Test
+	public void testSetMessage() {
+		four.setString("four");
+		assertEquals("four",four.getMessage());
+	}
+
+	@Test
+	public void testIsMessageEmpty() {
+		four.setString(null);
+		assertEquals(true,four.isMessageEmpty());
+		four.setString("");
+		assertEquals(true,four.isMessageEmpty());
+		four.setString("four");
+		assertEquals(false,four.isMessageEmpty());
+	}
+}
